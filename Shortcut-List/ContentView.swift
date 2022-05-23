fc9a5124fe4efbf1c72930c44fe1ed8d9dc6953e
takumac <takumi.sakai.engineer@gmail.com>
2022-05-24 00:29:47 +0900

    Initial Commit

---
+++ Shortcut-List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shortcut-List
+//
+//  Created by Takumi Sakai on 2022/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shortcut-List/Shortcut_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shortcut_ListApp.swift
+//  Shortcut-List
+//
+//  Created by Takumi Sakai on 2022/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Shortcut_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
